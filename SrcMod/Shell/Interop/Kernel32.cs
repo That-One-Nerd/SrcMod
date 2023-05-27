d6da6ca598aaa7ebfe3536b03efb098815bed6ad
That-One-Nerd <crazymailbro2@yahoo.com>
2023-05-27 16:34:55 -0400

    Added the GetConsoleWindow function.

--- SrcMod/Shell/Interop/Kernel32.cs
+++ SrcMod/Shell/Interop/Kernel32.cs
@@ -7,6 +7,9 @@ internal static partial class Kernel32
     public static partial bool GetConsoleScreenBufferInfo(nint hConsoleOutput, out ConsoleScreenBufferInfo lpConsoleScreenBufferInfo);
 
     [LibraryImport("kernel32.dll", SetLastError = true)]
+    public static partial nint GetConsoleWindow();
+
+    [LibraryImport("kernel32.dll", SetLastError = true)]
     public static partial uint GetFinalPathNameByHandleA(nint hFile, [MarshalAs(UnmanagedType.LPTStr)] string lpszFilePath,
         uint cchFilePath, uint dwFlags);
 
